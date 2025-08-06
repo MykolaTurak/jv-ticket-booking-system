eba8c4d2f34808b0853a3cbd6ba5fdee460b9a09
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-08-06 20:38:34 +0300

    solve task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,36 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
+    private int totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.totalSeats = totalSeats;
+        semaphore = new Semaphore(1);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        boolean success = false;
+        String message = "No seats available.";
+
+        try {
+            semaphore.acquire();
+            try {
+                if (totalSeats > 0) {
+                    totalSeats--;
+                    success = true;
+                    message = "Booking successful.";
+                }
+            } finally {
+                semaphore.release();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new BookingResult(user, success, message);
     }
+
 }
